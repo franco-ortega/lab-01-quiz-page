8c81b4a70395d721e041a6e34ece2da6a3441253
Franco Ortega <francopdx@gmail.com>
2020-09-29 00:35:03 -0700

    clean up code

--- README.md
+++ README.md
@@ -8,3 +8,51 @@
 
 + Give **ID** to BUTTON = button-start
 + Give **ID** to SPAN = span-results
+
+
+## JavaScipt
+
+When BUTTON is clicked, a POPUP will display
+
+
+USER begins QUIZ with a SCORE (TOTAL) of 0
+
+POPUP will ask user if they want to proceed with the QUIZ
+    -If user clicks YES, the QUIZ proceeds.
+        -POPUP moves to user NAME query.
+    -If user clicks NO, the QUIZ ends.
+        -POPUP closes.
+
+NAME query
+    -POPUP will ask user their NAME
+    -user NAME will be input into the QUESTIONS
+    -user NAME will be saved
+    -POPUP will move to QUESTION 1
+
+QUESTION 1
+    -POPUP uses NAME to ask QUESTION 1
+        -Correct ANSWER increases SCORE by 1 point
+        -Wrong ANSWER does not affect SCORE
+    -POPUP moves to Question 2
+
+
+QUESTION 2
+    -POPUP uses NAME to ask QUESTION 2
+        -Correct ANSWER increases SCORE by 1 point
+        -Wrong ANSWER does not affect SCORE
+    -POPUP moves to Question 2
+
+
+QUESTION 3
+    -POPUP uses NAME to ask QUESTION 3
+        -Correct ANSWER increases SCORE by 1 point
+        -Wrong ANSWER does not affect SCORE
+    -POPUP closes
+    -RESULTs display in SPAN
+
+RESULTS
+     -Display SCORE ('Your total is SCORE out of 3')
+     -If SCORE is 3, then 100%!!!
+     -If SCORE is 2, then PRETTY GOOD!!
+     -If SCORE is 1, then NOT BAD!
+     -If SCORE is 0, then BETTER LUCK NEXT TIME
--- app.js
+++ app.js
@@ -1,154 +1,45 @@
-/*
-
-When BUTTON is clicked, a POPUP will display
-
-
-USER begins QUIZ with a SCORE (TOTAL) of 0
-
-POPUP will ask user if they want to proceed with the QUIZ
-    -If user clicks YES, the QUIZ proceeds.
-        -POPUP moves to user NAME query.
-    -If user clicks NO, the QUIZ ends.
-        -POPUP closes.
-
-NAME query
-    -POPUP will ask user their NAME
-    -user NAME will be input into the QUESTIONS
-    -user NAME will be saved
-    -POPUP will move to QUESTION 1
-
-QUESTION 1
-    -POPUP uses NAME to ask QUESTION 1
-        -Correct ANSWER increases SCORE by 1 point
-        -Wrong ANSWER does not affect SCORE
-    -POPUP moves to Question 2
-
-
-QUESTION 2
-    -POPUP uses NAME to ask QUESTION 2
-        -Correct ANSWER increases SCORE by 1 point
-        -Wrong ANSWER does not affect SCORE
-    -POPUP moves to Question 2
-
-
-QUESTION 3
-    -POPUP uses NAME to ask QUESTION 3
-        -Correct ANSWER increases SCORE by 1 point
-        -Wrong ANSWER does not affect SCORE
-    -POPUP closes
-    -RESULTs display in SPAN
-
-RESULTS
-     -Display SCORE ('Your total is SCORE out of 3')
-     -If SCORE is 3, then 100%!!!
-     -If SCORE is 2, then PRETTY GOOD!!
-     -If SCORE is 1, then NOT BAD!
-     -If SCORE is 0, then BETTER LUCK NEXT TIME
-
-*/
-
 import { countsAsYes } from './count-as-yes.js'
-
 
 let quizScore = 0
 
-        /*console.log(quizScore);*/
-
 const spanResults = document.getElementById('span-results');
-
 const buttonStart = document.getElementById('button-start');
-
-        /*console.log(buttonStart);*/
-
-
 
 buttonStart.addEventListener('click', () => {
 
-        /*console*/console.log('the button was clicked!');
-        /*console*/console.log(quizScore);
-
-
-
     const quizApproval = confirm('Do you want to take this quiz?');
 
-        /*console*/console.log(quizApproval);
-        /*console*/console.log(quizScore);
-
     if (quizApproval === true) {
-    
-    const userName = prompt('What is your name?')
-
-        /*console*/console.log(userName);
-
+        const userName = prompt('What is your name?')
 
 
 /* START of QUESTION 1 */
-    const answerOne = prompt(`Hey ${userName}! Does Lavinia get married?`);
-
-        /*console*/console.log(answerOne);
-        /*console*/console.log(quizScore);
-    
+        const answerOne = prompt(`Hey ${userName}! Does Lavinia get married?`);
         const judgeOne = countsAsYes(answerOne);
         
-        /*console*/console.log(answerOne);
-        /*console*/console.log(quizScore);
-
             if (judgeOne === true) {
                 quizScore++;
-
-        /*console*/console.log(quizScore);
-
-            } else {
-        /*console*/console.log('no points');
-
             }
-/* END of QUESTION 1*/                
 
-
-/* START of QUESTION 2*/
-        const answerTwo = prompt(`Hey ${userName}! Is the new found city called Lantuma?`);
-
-        /*console*/console.log(answerTwo);
-        /*console*/console.log(quizScore);
-
+/* START of QUESTION 2 */
+        const answerTwo = prompt(`How about this one, ${userName}. Is the new found city called Lantuma?`);
         const judgeTwo = countsAsYes(answerTwo);
     
-        /*console*/console.log(answerTwo);
-        /*console*/console.log(quizScore);
-    
-        if (judgeTwo === false) {
-            quizScore++;
-    
-        /*console*/console.log(quizScore + ' another point!');
-    
-            } else {
-
+            if (judgeTwo === false) {
+                quizScore++;
             }
-/* END of QUESTION 2*/
 
-
-/* START of QUESTION 3*/
-const answerThree = prompt(`Hey ${userName}! Does Lavinia turn into a cat?`);
-
-    /*console*/console.log(answerThree);
-    /*console*/console.log(quizScore);
-
-    const judgeThree = countsAsYes(answerThree);
+/* START of QUESTION 3 */
+        const answerThree = prompt(`One more to go, ${userName}! Does Lavinia turn into a cat?`);
+        const judgeThree = countsAsYes(answerThree);
     
-    /*console*/console.log(answerThree);
-    /*console*/console.log(quizScore);
-        
-        if (judgeThree === false) {
-        quizScore++;
-        
-    /*console*/console.log(quizScore + ' another point!');
-
-        } else {
-        
+            if (judgeThree === false) {
+                quizScore++;
         }
-/* END of QUESTION 3*/
-    
 
+        alert(`You have completed the quiz, ${userName}! Click OK to see your results`);
+    
+/* QUIZ SCORE */
     if (quizScore === 3) {
         spanResults.textContent = `Great job, ${userName}! You correctly answered all ${quizScore} out of 3!!`;
     } else if (quizScore === 2) {
@@ -161,10 +52,6 @@ const answerThree = prompt(`Hey ${userName}! Does Lavinia turn into a cat?`);
 
     quizScore = 0;
 
-    } else {
-        return;
-    }
-    
-
+    } 
 });
 
